a642711049890013fecccf8c351ad3b21299e3ec
garrett <garrettbowman96@gmail.com>
2025-04-22 00:08:59 -0400

    created mainpage

---
+++ main.js
@@ -0,0 +1,19 @@
+/*************  ✨ Windsurf Command ⭐  *************/
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+/*******  2f98899a-4a38-4107-ae19-749277cc2c24  *******/
