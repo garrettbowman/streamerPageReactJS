b4c2fe3ec5be2bddffc7bd56b37d908f1abc5c63
garrett <garrettbowman96@gmail.com>
2025-05-07 17:02:48 -0400

    --

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ class App extends React.Component {
     return (
       <div>
         <h1>Hello World!</h1>
-        <p>This is a paragraph.</p>
+        <p>This is the paragraph.</p>
       </div>
     );
   }
